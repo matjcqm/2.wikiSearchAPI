6c06c5d6abbac3dcc9dccfffd79201a9ef6baac6
matjcqm <mathieu.jacquemin@hotmail.com>
2023-03-11 10:23:45 +0100

    JS OK

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const errorText = document.querySelector(".errorText");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  if (!input.value === "") {
+    errorText.textContent = "Wops, veuillez remplir l'input";
+    return;
+  } else {
+    errorText.textContent = "";
+    wikiApiCall(input.value);
+  }
+}
+
+async function wikiApiCall(searchInput) {
+  const response = await fetch(
+    `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+  );
+
+  const data = await response.json();
+  createCards(data.query.search);
+}
+
+const resultsDisplay = document.querySelector(".resultsDisplay");
+
+function createCards(data) {
+  if (!data.length) {
+    errorText.textContent = "Pas de résultat";
+    return;
+  }
+  data.map((el) => {
+    const url = `https://en.wikipedia.org/?curid=${el.pageid}`;
+    const card = document.createElement("div");
+    card.className = "result-item";
+    card.innerHTML = `
+        <h3 class="result-tile">
+            <a href=${url} target="_blank">${el.title}</a>
+        </h3>
+        <a href=${url} class="result-link" target="_blank">${url}</a>
+        <span class="result-snippet">${el.snippet}</span>
+        <br>
+        `;
+
+    resultsDisplay.appendChild(card);
+  });
+}
